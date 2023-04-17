bafe1310747e5db901505c702ac0bb2bb856c714
Manoug Pfennig <mapfenni@c2r1p1.42nice.fr>
2023-04-17 19:04:48 +0200

    hola

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: mapfenni <mapfenni@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/27 18:50:08 by mapfenni          #+#    #+#             */
-/*   Updated: 2023/04/11 19:07:37 by mapfenni         ###   ########.fr       */
+/*   Updated: 2023/04/17 19:03:33 by mapfenni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,40 +14,26 @@
 
 unsigned int	ft_strlcat(char *dest, char *src, unsigned int size)
 {
-	unsigned int	i;
-	unsigned int	j;
-	unsigned int	src_size;
-	unsigned int	dest_size;
+	unsigned int	dst_len;
+	unsigned int	src_len;
 
-	i = 0;
-	j = 0;
+	src_len = (unsigned int)ft_strlen(src);
 	if (size == 0)
-		return (ft_strlen(src));
-	while (dest[j] != '\0')
-		j++;
-	dest_size = j;
-	while (src[i++] != '\0')
-		src_size = i;
-	i = 0;
-	if (size == 0 || size <= dest_size)
-		return (src_size + size);
-	while (src[i] != '\0' && i < (size - dest_size - 1))
-	{
-		dest[j] = src[i];
-		i++;
-		j++;
-	}
-	dest[j] = '\0';
-	return (src_size + dest_size);
+		return (src_len);
+	dst_len = (unsigned int)ft_strlen(dest);
+	if (dst_len >= size)
+		return (src_len + size);
+	dest += dst_len;
+	size -= dst_len;
+	while (*src && --size)
+		*dest++ = *src++;
+	*dest = '\0';
+	return (dst_len + src_len);
 }
 
 // #include <string.h>
 // #include <stdio.h>
 // int main()
 // {
-// 	char *src = (char *)"AAAAAAAAA";
-// 	char dest[30];
-// 	memset(dest, 0, 30);
-// 	dest[0] = 'B';
-// 	printf("%lu", strlcat(dest, "123", 0));
+// 	printf("%u", ft_strlcat("toto", "", 10));
 // }
